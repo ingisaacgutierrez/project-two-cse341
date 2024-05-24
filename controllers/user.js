5e4b5528ac51e10a786e56809987df6b70db69c5
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2024-05-24 18:01:09 -0500

    fix user name getsingle

--- controllers/user.js
+++ controllers/user.js
@@ -10,12 +10,12 @@ result.toArray().then((lists) => {
 };
 
 const getSingle = async (req, res) => {
-const userId = new ObjectId(req.params.id);
+const userName = req.params.username;
 const result = await mongodb
     .getDb()
     .db()
     .collection('user')
-    .find({ _id: userId });
+    .findOne({ 'user.username': userName });
 result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
