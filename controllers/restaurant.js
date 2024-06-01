6903aa32ed2ded4916adf853694b0f637ccf06b2
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2024-06-01 17:58:41 -0500

    Validation and error handling

--- controllers/restaurant.js
+++ controllers/restaurant.js
@@ -1,98 +1,163 @@
+const { body, validationResult, param } = require('express-validator');
 const mongodb = require('../db/connect');
 
 const getAll = async (req, res) => {
-  const result = await mongodb.getDb().db().collection('restaurant').find();
-  result.toArray().then((lists) => {
+  try {
+    const result = await mongodb.getDb().db().collection('restaurant').find();
+    const lists = await result.toArray();
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
-  });
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred while retrieving the restaurants', error });
+  }
 };
 
 const getSingle = async (req, res) => {
   const restaurantName = req.params.name;
-  const result = await mongodb
-    .getDb()
-    .db()
-    .collection('restaurant')
-    .findOne({ name: restaurantName });
+  try {
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection('restaurant')
+      .findOne({ name: restaurantName });
 
-  if (result) {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(result);
-  } else {
-    res.status(404).json({ message: 'User Not Found' });
-  }
-};
-
-const newRestaurant = async (req, res) => {
-  const restaurantInsert = {
-    name: req.body.name,
-    address: {
-      street: req.body.address.street,
-      city: req.body.address.city,
-      state: req.body.address.state,
-      zipCode: req.body.address.zipCode
-    },
-    phoneNumber: req.body.phoneNumber,
-    menu: {
-      burgers: req.body.menu.burgers,
-      fries: req.body.menu.fries,
-      drinks: req.body.menu.drinks,
-      desserts: req.body.menu.desserts
+    if (result) {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ message: 'Restaurant Not Found' });
     }
-  };
-  const response = await mongodb.getDb().db().collection('restaurant').insertOne(restaurantInsert);
-  if (response.acknowledged) {
-    res.status(201).json(response);
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while creating the user.');
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred while retrieving the restaurant', error });
   }
 };
 
-const updateRestaurant = async (req, res) => {
-  const restaurantName = req.params.name;
-  const restaurantInsert = {
-    name: req.body.name,
-    address: {
-      street: req.body.address.street,
-      city: req.body.address.city,
-      state: req.body.address.state,
-      zipCode: req.body.address.zipCode
-    },
-    phoneNumber: req.body.phoneNumber,
-    menu: {
-      burgers: req.body.menu.burgers,
-      fries: req.body.menu.fries,
-      drinks: req.body.menu.drinks,
-      desserts: req.body.menu.desserts
+const newRestaurant = [
+  body('name').notEmpty().withMessage('Name is required'),
+  body('address.street').notEmpty().withMessage('Street is required'),
+  body('address.city').notEmpty().withMessage('City is required'),
+  body('address.state').notEmpty().withMessage('State is required'),
+  body('address.zipCode').notEmpty().withMessage('Zip code is required'),
+  body('phoneNumber').notEmpty().withMessage('Phone number is required'),
+  body('menu.burgers').isArray().withMessage('Burgers must be an array'),
+  body('menu.fries').isArray().withMessage('Fries must be an array'),
+  body('menu.drinks').isArray().withMessage('Drinks must be an array'),
+  body('menu.desserts').isArray().withMessage('Desserts must be an array'),
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  };
-  const response = await mongodb
-    .getDb()
-    .db()
-    .collection('restaurant')
-    .replaceOne({ name: restaurantName }, restaurantInsert);
-  console.log(response);
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
-  }
-};
 
-const deleteRestaurant = async (req, res) => {
-  const deleteName = req.params.name;
-  const response = await mongodb
-    .getDb()
-    .db()
-    .collection('restaurant')
-    .deleteOne({ name: deleteName }, true);
-  console.log(response);
-  if (response.deletedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+    const restaurantInsert = {
+      name: req.body.name,
+      address: {
+        street: req.body.address.street,
+        city: req.body.address.city,
+        state: req.body.address.state,
+        zipCode: req.body.address.zipCode
+      },
+      phoneNumber: req.body.phoneNumber,
+      menu: {
+        burgers: req.body.menu.burgers,
+        fries: req.body.menu.fries,
+        drinks: req.body.menu.drinks,
+        desserts: req.body.menu.desserts
+      }
+    };
+
+    try {
+      const response = await mongodb
+        .getDb()
+        .db()
+        .collection('restaurant')
+        .insertOne(restaurantInsert);
+      if (response.acknowledged) {
+        res.status(201).json(response);
+      } else {
+        res.status(500).json({ message: 'An error occurred while creating the restaurant.' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'An error occurred while creating the restaurant', error });
+    }
   }
-};
+];
+
+const updateRestaurant = [
+  param('name').notEmpty().withMessage('Restaurant name is required'),
+  body('name').notEmpty().withMessage('Name is required'),
+  body('address.street').notEmpty().withMessage('Street is required'),
+  body('address.city').notEmpty().withMessage('City is required'),
+  body('address.state').notEmpty().withMessage('State is required'),
+  body('address.zipCode').notEmpty().withMessage('Zip code is required'),
+  body('phoneNumber').notEmpty().withMessage('Phone number is required'),
+  body('menu.burgers').isArray().withMessage('Burgers must be an array'),
+  body('menu.fries').isArray().withMessage('Fries must be an array'),
+  body('menu.drinks').isArray().withMessage('Drinks must be an array'),
+  body('menu.desserts').isArray().withMessage('Desserts must be an array'),
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const restaurantName = req.params.name;
+    const restaurantInsert = {
+      name: req.body.name,
+      address: {
+        street: req.body.address.street,
+        city: req.body.address.city,
+        state: req.body.address.state,
+        zipCode: req.body.address.zipCode
+      },
+      phoneNumber: req.body.phoneNumber,
+      menu: {
+        burgers: req.body.menu.burgers,
+        fries: req.body.menu.fries,
+        drinks: req.body.menu.drinks,
+        desserts: req.body.menu.desserts
+      }
+    };
+
+    try {
+      const response = await mongodb
+        .getDb()
+        .db()
+        .collection('restaurant')
+        .replaceOne({ name: restaurantName }, restaurantInsert);
+      if (response.modifiedCount > 0) {
+        res.status(204).send();
+      } else {
+        res.status(500).json({ message: 'An error occurred while updating the restaurant.' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'An error occurred while updating the restaurant', error });
+    }
+  }
+];
+
+const deleteRestaurant = [
+  param('name').notEmpty().withMessage('Restaurant name is required'),
+
+  async (req, res) => {
+    const deleteName = req.params.name;
+    try {
+      const response = await mongodb
+        .getDb()
+        .db()
+        .collection('restaurant')
+        .deleteOne({ name: deleteName });
+      if (response.deletedCount > 0) {
+        res.status(204).send();
+      } else {
+        res.status(500).json({ message: 'An error occurred while deleting the restaurant.' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'An error occurred while deleting the restaurant', error });
+    }
+  }
+];
 
 module.exports = { getAll, getSingle, newRestaurant, updateRestaurant, deleteRestaurant };
--- controllers/user.js
+++ controllers/user.js
@@ -1,112 +1,226 @@
+const { body, validationResult, param } = require('express-validator');
 const mongodb = require('../db/connect');
+const passwordUtil = require('../utils/passwordComplexityCheck');
 
 const getAll = async (req, res) => {
-  const result = await mongodb.getDb().db().collection('user').find();
-  result.toArray().then((lists) => {
+  try {
+    const result = await mongodb.getDb().db().collection('user').find();
+    const lists = await result.toArray();
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
-  });
-};
-
-const getSingle = async (req, res) => {
-  const username = req.params.username;
-  const result = await mongodb.getDb().db().collection('user').findOne({ username: username });
-
-  if (result) {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(result);
-  } else {
-    res.status(404).json({ message: 'User Not Found' });
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred while retrieving the users', error });
   }
 };
 
-const newUser = async (req, res) => {
-  const userInsert = {
-    username: req.body.username,
-    password: req.body.password,
-    email: req.body.email,
-    phoneNumber: req.body.phoneNumber,
-    deliveryAddress: {
-      street: req.body.deliveryAddress.street,
-      city: req.body.deliveryAddress.city,
-      state: req.body.deliveryAddress.state,
-      zipCode: req.body.deliveryAddress.zipCode
-    },
-    restaurant: {
-      name: req.body.restaurant.name
-    },
-    order: {
-      burgers: req.body.order.burgers,
-      fries: req.body.order.fries,
-      drinks: req.body.order.drinks,
-      dessert: req.body.order.dessert
-    },
-    paymentMethod: {
-      cash: req.body.paymentMethod.cash,
-      creditCard: req.body.paymentMethod.creditCard
+const getSingle = [
+  param('username').notEmpty().withMessage('Username is required'),
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  };
-  const response = await mongodb.getDb().db().collection('user').insertOne(userInsert);
-  if (response.acknowledged) {
-    res.status(201).json(response);
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while creating the user.');
-  }
-};
 
-const updateUser = async (req, res) => {
-  const userName = req.params.username;
-  const userInsert = {
-    username: req.body.username,
-    password: req.body.password,
-    email: req.body.email,
-    phoneNumber: req.body.phoneNumber,
-    deliveryAddress: {
-      street: req.body.deliveryAddress.street,
-      city: req.body.deliveryAddress.city,
-      state: req.body.deliveryAddress.state,
-      zipCode: req.body.deliveryAddress.zipCode
-    },
-    restaurant: {
-      name: req.body.restaurant.name
-    },
-    order: {
-      burgers: req.body.order.burgers,
-      fries: req.body.order.fries,
-      drinks: req.body.order.drinks,
-      dessert: req.body.order.dessert
-    },
-    paymentMethod: {
-      cash: req.body.paymentMethod.cash,
-      creditCard: req.body.paymentMethod.creditCard
+    const username = req.params.username;
+    try {
+      const result = await mongodb.getDb().db().collection('user').findOne({ username: username });
+
+      if (result) {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(result);
+      } else {
+        res.status(404).json({ message: 'User Not Found' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'An error occurred while retrieving the user', error });
     }
-  };
-  const response = await mongodb
-    .getDb()
-    .db()
-    .collection('user')
-    .replaceOne({ username: userName }, userInsert);
-  console.log(response);
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
   }
+];
+
+const validatePassword = (password) => {
+  const { error } = passwordUtil.passwordPass(password);
+  if (error) {
+    return error.details[0].message;
+  }
+  return null;
 };
 
-const deleteUser = async (req, res) => {
-  const userName = req.params.username;
-  const response = await mongodb
-    .getDb()
-    .db()
-    .collection('user')
-    .deleteOne({ username: userName }, true);
-  console.log(response);
-  if (response.deletedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+const newUser = [
+  body('username').notEmpty().withMessage('Username is required'),
+  body('password')
+    .notEmpty()
+    .withMessage('Password is required')
+    .custom((value) => {
+      const passwordError = validatePassword(value);
+      if (passwordError) {
+        throw new Error(passwordError);
+      }
+      return true;
+    }),
+  body('email').isEmail().withMessage('Email is required and must be valid'),
+  body('phoneNumber').notEmpty().withMessage('Phone number is required'),
+  body('deliveryAddress.street').notEmpty().withMessage('Street is required'),
+  body('deliveryAddress.city').notEmpty().withMessage('City is required'),
+  body('deliveryAddress.state').notEmpty().withMessage('State is required'),
+  body('deliveryAddress.zipCode').notEmpty().withMessage('Zip code is required'),
+  body('restaurant.name').notEmpty().withMessage('Restaurant name is required'),
+  body('order.burgers').notEmpty().withMessage('Burgers must be an array'),
+  body('order.fries').notEmpty().withMessage('Fries must be an array'),
+  body('order.drinks').notEmpty().withMessage('Drinks must be an array'),
+  body('order.dessert').notEmpty().withMessage('Dessert must be an array'),
+  body('paymentMethod.cash').isBoolean().withMessage('Cash must be a boolean'),
+  body('paymentMethod.creditCard').isBoolean().withMessage('Credit card must be a boolean'),
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const userInsert = {
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+      phoneNumber: req.body.phoneNumber,
+      deliveryAddress: {
+        street: req.body.deliveryAddress.street,
+        city: req.body.deliveryAddress.city,
+        state: req.body.deliveryAddress.state,
+        zipCode: req.body.deliveryAddress.zipCode
+      },
+      restaurant: {
+        name: req.body.restaurant.name
+      },
+      order: {
+        burgers: req.body.order.burgers,
+        fries: req.body.order.fries,
+        drinks: req.body.order.drinks,
+        dessert: req.body.order.dessert
+      },
+      paymentMethod: {
+        cash: req.body.paymentMethod.cash,
+        creditCard: req.body.paymentMethod.creditCard
+      }
+    };
+
+    try {
+      const response = await mongodb.getDb().db().collection('user').insertOne(userInsert);
+      if (response.acknowledged) {
+        res.status(201).json(response);
+      } else {
+        res.status(500).json({ message: 'An error occurred while creating the user.' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'An error occurred while creating the user', error });
+    }
   }
-};
+];
+
+const updateUser = [
+  param('username').notEmpty().withMessage('Username is required'),
+  body('username').notEmpty().withMessage('Username is required'),
+  body('password')
+    .notEmpty()
+    .withMessage('Password is required')
+    .custom((value) => {
+      const passwordError = validatePassword(value);
+      if (passwordError) {
+        throw new Error(passwordError);
+      }
+      return true;
+    }),
+  body('password').notEmpty().withMessage('Password is required'),
+  body('email').isEmail().withMessage('Email is required and must be valid'),
+  body('phoneNumber').notEmpty().withMessage('Phone number is required'),
+  body('deliveryAddress.street').notEmpty().withMessage('Street is required'),
+  body('deliveryAddress.city').notEmpty().withMessage('City is required'),
+  body('deliveryAddress.state').notEmpty().withMessage('State is required'),
+  body('deliveryAddress.zipCode').notEmpty().withMessage('Zip code is required'),
+  body('restaurant.name').notEmpty().withMessage('Restaurant name is required'),
+  body('order.burgers').notEmpty().withMessage('Burgers must be an array'),
+  body('order.fries').notEmpty().withMessage('Fries must be an array'),
+  body('order.drinks').notEmpty().withMessage('Drinks must be an array'),
+  body('order.dessert').notEmpty().withMessage('Dessert must be an array'),
+  body('paymentMethod.cash').isBoolean().withMessage('Cash must be a boolean'),
+  body('paymentMethod.creditCard').isBoolean().withMessage('Credit card must be a boolean'),
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const userName = req.params.username;
+    const userInsert = {
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+      phoneNumber: req.body.phoneNumber,
+      deliveryAddress: {
+        street: req.body.deliveryAddress.street,
+        city: req.body.deliveryAddress.city,
+        state: req.body.deliveryAddress.state,
+        zipCode: req.body.deliveryAddress.zipCode
+      },
+      restaurant: {
+        name: req.body.restaurant.name
+      },
+      order: {
+        burgers: req.body.order.burgers,
+        fries: req.body.order.fries,
+        drinks: req.body.order.drinks,
+        dessert: req.body.order.dessert
+      },
+      paymentMethod: {
+        cash: req.body.paymentMethod.cash,
+        creditCard: req.body.paymentMethod.creditCard
+      }
+    };
+
+    try {
+      const response = await mongodb
+        .getDb()
+        .db()
+        .collection('user')
+        .replaceOne({ username: userName }, userInsert);
+      if (response.modifiedCount > 0) {
+        res.status(204).send();
+      } else {
+        res.status(500).json({ message: 'An error occurred while updating the user.' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'An error occurred while updating the user', error });
+    }
+  }
+];
+
+const deleteUser = [
+  param('username').notEmpty().withMessage('Username is required'),
+
+  async (req, res) => {
+    const userName = req.params.username;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const response = await mongodb
+        .getDb()
+        .db()
+        .collection('user')
+        .deleteOne({ username: userName });
+      if (response.deletedCount > 0) {
+        res.status(204).send();
+      } else {
+        res.status(500).json({ message: 'An error occurred while deleting the user.' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'An error occurred while deleting the user', error });
+    }
+  }
+];
 
 module.exports = { getAll, getSingle, newUser, updateUser, deleteUser };
