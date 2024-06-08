0ecbc6e255968b41ff2caa66f1c13264150d21a7
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2024-06-07 22:39:56 -0500

    first changes

--- server.js
+++ server.js
@@ -5,6 +5,25 @@ const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
 const app = express();
 
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: 'https://project-two-cse341.onrender.com',
+  clientID: 'EgRbFHcAzLGmC6Ki9Kl8Jn3Ma0p64EAD',
+  issuerBaseURL: 'https://dev-2cl6obyj0ux6ly40.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
