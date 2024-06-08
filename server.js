5afbc08d2fed972543193c36a694e7e93a8fedea
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2024-06-07 22:34:07 -0500

    change env file

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const config = {
   // auth router attaches /login, /logout, and /callback routes to the baseURL
   app.use(auth(config));
   
-  // req.isAuthenticated is provided from the auth router
+  // req.isAuthenticated is provided from the auth 
   app.get('/', (req, res) => {
     res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
   });
